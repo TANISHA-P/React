b64f944d4506c7f531ebc4a57e568937fd3518b9
TANISHA-P <tanishapareek2002@gmail.com>
2024-05-17 19:30:45 +0530

    creating DOM structure

--- R1/mew.js
+++ R1/mew.js
@@ -5,4 +5,24 @@ const heading = React.createElement(
 const heading2 = React.createElement("h5",{},"small data");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+
+// creating a DOM structure
+// <div>
+//      <div>
+//          <h1> I am h1 tag </h1>
+//      </div>
+// </div>
+const parentdiv = React.createElement(
+    "div",
+    {id : "parent"},
+    React.createElement(
+        "div",
+        {id : "child"},
+        React.createElement(
+            "h1",
+            {},
+            "I am h1 tag"
+        )
+    )
+)
+root.render(parentdiv);
