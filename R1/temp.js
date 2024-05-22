d232d346919bf1e3da8ecdec4bb9796860093c13
TANISHA-P <tanishapareek2002@gmail.com>
2024-05-22 18:36:29 +0530

    Adding components

--- R1/temp.js
+++ R1/temp.js
@@ -4,6 +4,17 @@ import ReactDOM from "react-dom/client"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const firstLine = <h1>Hello Moto</h1>
 
+const TempComp = () => (
+    <div id = "t1">
+        <Title/>
+        <h1> Hi</h1>
+        <div className="t2">
+            <h2>Myself Tanisha</h2>
+        </div>
+    </div>
+);
+
+const Title = () => <h1>I am Title</h1>;
 const temp = (
     <div id = "t1">
         <h1> Hi</h1>
@@ -13,4 +24,4 @@ const temp = (
     </div>
 )
 
-root.render(temp);
+root.render(<TempComp/>);
