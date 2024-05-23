918909671855b361feb7c24f4aac20fe3d85d6cb
TANISHA-P <tanishapareek2002@gmail.com>
2024-05-23 12:09:42 +0530

    Initialised food app

---
+++ R2/App.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const App = () => {
+    
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App/>)
